fe82f7edfafeaf021f2e027383f077cfeb4abc42
danik529 <lsy12011@naver.com>
2018-11-08 15:46:03 +0900

    07_pointer2

--- main.c
+++ main.c
@@ -4,14 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i = 365;
-	int *ptr = &i;
+	int x=10;
+	int *ptr;
 	
-	printf("i address : %i\n", &i);
-	printf("i value : %i\n", i);
+	ptr=&x;
+	printf("ptr value : %d\n", *ptr);
 	
-	printf("ptr address : %i\n", ptr);
-	printf("ptr value : %i\n", *ptr);
+	ptr=&x;
+	*ptr=20;
+	printf("x value : %d\n", x);
 	
 	return 0;
 }
