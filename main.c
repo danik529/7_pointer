568c569df666006eaeddb1164d96e18685352efa
danik529 <lsy12011@naver.com>
2018-11-08 15:54:15 +0900

    07_pointer2-correctfile

--- main.c
+++ main.c
@@ -5,14 +5,18 @@
 
 int main(int argc, char *argv[]) {
 	int x=10;
+	int y=20; 
 	int *ptr;
 	
 	ptr=&x;
-	printf("ptr value : %d\n", *ptr);
+	printf("x : %i\n", *ptr);
 	
-	ptr=&x;
-	*ptr=20;
-	printf("x value : %d\n", x);
+	ptr=&y;
+	printf("y : %i\n", *ptr);
+	
+	ptr = &x;
+	*ptr = 20;
+	printf("x : %i\n", x);
 	
 	return 0;
-}
+}  
